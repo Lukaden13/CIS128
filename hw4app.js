b8c72a7ec1ecc2db161bce8be748c32fe099a601
Lukaden13 <128631623+Lukaden13@users.noreply.github.com>
2024-04-30 08:59:52 -0700

    Update hw4app.js

--- hw4app.js
+++ hw4app.js
@@ -1,15 +1,7 @@
 function applyBackground() {
     var background = document.getElementById("background-select").value;
-    switch (background) {
-        case "lightblue":
-            document.body.style.backgroundImage = "url('images/lions.jpg')";
-            break;
-        case "lightgold":
-            document.body.style.backgroundImage = "url('images/lionswidth400.jpg')";
-            break;
-        default:
-            document.body.style.backgroundImage = "none";  // No background image
-    }
+    document.body.style.backgroundImage = `url('images/${background}.jpg')`;
+}
 
 function updateName() {
     const name = document.getElementById("username").value;
